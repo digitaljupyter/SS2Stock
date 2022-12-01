d9bde7a34f492c6c6a4d3eb1ab0245aec1bbeea1
Kai Daniel Gonzalez <gkai70264@gmail.com>
2022-11-30 19:45:15 -0500

    Send to webhook

--- Auto.py
+++ Auto.py
@@ -8,6 +8,8 @@ fluctuation = json.load(open("Fluctuate.json", "r"))
 
 fluctuation_amount = random.randrange(fluctuation["start"], fluctuation["end"])
 
+old_amount = json.load(open("Currency.json", "r"))["networth"]
+
 print("Fluctuating to: " + str(fluctuation_amount))
 
 with open("Currency.json", "w") as f:
@@ -24,5 +26,14 @@ print("Commiting to git")
 os.system("git add Currency.json")
 
 os.system("git commit -m \"[auto] update for " + datetime.datetime.now().strftime("%m/%d/%Y") + "\"")
+
+## Add updates
+
+from discord_webhook import DiscordWebhook
+
+url = json.load(open("env.json"))["url"]
+webhook = DiscordWebhook(url=url, content='Update to SimBuck value! Old: **' + str(old_amount) + "**\nNew: **" + str(fluctuation_amount) + "**\nValue went ***" +
+  ("Down" if fluctuation_amount < old_amount else "Up") + "***!")
+response = webhook.execute()
 
 # os.system("git push")
